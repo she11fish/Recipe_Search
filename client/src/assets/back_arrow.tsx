c225ac7d53d20d322dc7dbddd3c367953e37cda7
she11fish <trolltrolltrolllollollol@outlook.com>
2022-08-16 06:18:53 -0400

    uploading files

---
+++ client/src/assets/back_arrow.tsx
@@ -0,0 +1,7 @@
+export default function BackArrow() {
+    return (
+        <svg width="30" height="26" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.25 13H28.25M12.5 24.25L1.25 13L12.5 24.25ZM1.25 13L12.5 1.75L1.25 13Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
